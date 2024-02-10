072aa23fe141a6205b5ab88bc9fc36235460f4cb
Alex Bates <alexbates.dev@gmail.com>
2024-02-10 16:06:27 -0700

    Laying Out Plans

--- frontend/js/profile.js
+++ frontend/js/profile.js
@@ -3,8 +3,13 @@ const handleClickPiece = (event) => {
     // Open up a menu allowing the user to edit the piece's text
     // (and possibly view per-piece stats)
     // ONLY AVAILABLE TO VICTORS
+    console.log(event.target);
 };
 
+const handleLogoutClicked = () => {
+    document.cookie = null;
+    window.location.href = 'login.html'
+}
 
 const handleEditClicked = () => {
     alert('Edit clicked');
@@ -23,5 +28,16 @@ const handleDeleteClicked = () => {
 
 window.onload = () => {
     // Get the user's profile
-    // Initialize the interactable JavaScript bit
+    // Initialize the interactable JavaScript bits
+    let pieces = document.getElementsByClassName('piece');
+    let itter_count = 0;
+    for (let piece of pieces) {
+        piece.addEventListener('click', handleClickPiece);
+        if (itter_count < 12) {
+            piece.id = `${itter_count + 1}-A`;
+        } else {
+            piece.id = `${itter_count - 11}-B`;
+        }
+        itter_count++;
+    }
 }
