39dba77763a1e3766a8dc68c8a1d0aa90c74aa4b
Alex Bates <alexbates.dev@gmail.com>
2024-02-10 17:17:53 -0700

    Unplanned Frontend Changes!

--- frontend/js/board.js
+++ frontend/js/board.js
@@ -135,177 +135,173 @@ const undo = () => {
 
 window.onload = () => {
 
-    if (document.cookie == null || document.cookie == "") {
-        Game = {
-            players: {
-                A: {
-                    pieces: ["OwO", "UwU", "EwE", "Salo", "Fridge", "🐈", "🦈", "0 - 0", ">w>", "Squij", "🐛", "Pheeb"],
-                    primaryColor: "black",
-                    secondaryColor: "white",
-                },
-                B: {
-                    pieces: ["^_^", "(•_•)", "O.O", "^o^", "X_X", "ᓚᘏᗢ", "💀", "🐢", "/ᐠ｡ꞈ｡ᐟ\\", "Sheem", "Pinto", ":3"],
-                    primaryColor: "white",
-                    secondaryColor: "black",
-                },
+
+    Game = {
+        players: {
+            A: {
+                pieces: ["OwO", "UwU", "EwE", "Salo", "Fridge", "🐈", "🦈", "0 - 0", ">w>", "Squij", "🐛", "Pheeb"],
+                primaryColor: "black",
+                secondaryColor: "white",
             },
-            turnCount: 0,
-            board: [
-                [
-                    null,
-                    {
-                        id: "1-A",
-                        promoted: false,
-                    },
-                    null,
-                    {
-                        id: "2-A",
-                        promoted: false,
-                    },
-                    null,
-                    {
-                        id: "3-A",
-                        promoted: false,
-                    },
-                    null,
-                    {
-                        id: "4-A",
-                        promoted: false,
-                    },
-                ],
-                [
-                    {
-                        id: "5-A",
-                        promoted: false,
-                    },
-                    null,
-                    {
-                        id: "6-A",
-                        promoted: false,
-                    },
-                    null,
-                    {
-                        id: "7-A",
-                        promoted: false,
-                    },
-                    null,
-                    {
-                        id: "8-A",
-                        promoted: false,
-                    },
-                ],
-                [
-                    null,
-                    {
-                        id: "9-A",
-                        promoted: false,
-                    },
-                    null,
-                    {
-                        id: "10-A",
-                        promoted: false,
-                    },
-                    null,
-                    {
-                        id: "11-A",
-                        promoted: false,
-                    },
-                    null,
-                    {
-                        id: "12-A",
-                        promoted: false,
-                    },
-                ],
-                [
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                ],
-                [
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                ],
-                [
-                    {
-                        id: "1-B",
-                        promoted: false,
-                    },
-                    null,
-                    {
-                        id: "2-B",
-                        promoted: false,
-                    },
-                    null,
-                    {
-                        id: "3-B",
-                        promoted: false,
-                    },
-                    null,
-                    {
-                        id: "4-B",
-                        promoted: false,
-                    },
-                ],
-                [
-                    null,
-                    {
-                        id: "5-B",
-                        promoted: false,
-                    },
-                    null,
-                    {
-                        id: "6-B",
-                        promoted: false,
-                    },
-                    null,
-                    {
-                        id: "7-B",
-                        promoted: false,
-                    },
-                    null,
-                    {
-                        id: "8-B",
-                        promoted: false,
-                    },
-                ],
-                [
-                    {
-                        id: "9-B",
-                        promoted: false,
-                    },
-                    null,
-                    {
-                        id: "10-B",
-                        promoted: false,
-                    },
-                    null,
-                    {
-                        id: "11-B",
-                        promoted: false,
-                    },
-                    null,
-                    {
-                        id: "12-B",
-                        promoted: false,
-                    },
-                ]
+            B: {
+                pieces: ["^_^", "(•_•)", "O.O", "^o^", "X_X", "ᓚᘏᗢ", "💀", "🐢", "/ᐠ｡ꞈ｡ᐟ\\", "Sheem", "Pinto", ":3"],
+                primaryColor: "white",
+                secondaryColor: "black",
+            },
+        },
+        turnCount: 0,
+        board: [
+            [
+                null,
+                {
+                    id: "1-A",
+                    promoted: false,
+                },
+                null,
+                {
+                    id: "2-A",
+                    promoted: false,
+                },
+                null,
+                {
+                    id: "3-A",
+                    promoted: false,
+                },
+                null,
+                {
+                    id: "4-A",
+                    promoted: false,
+                },
+            ],
+            [
+                {
+                    id: "5-A",
+                    promoted: false,
+                },
+                null,
+                {
+                    id: "6-A",
+                    promoted: false,
+                },
+                null,
+                {
+                    id: "7-A",
+                    promoted: false,
+                },
+                null,
+                {
+                    id: "8-A",
+                    promoted: false,
+                },
+            ],
+            [
+                null,
+                {
+                    id: "9-A",
+                    promoted: false,
+                },
+                null,
+                {
+                    id: "10-A",
+                    promoted: false,
+                },
+                null,
+                {
+                    id: "11-A",
+                    promoted: false,
+                },
+                null,
+                {
+                    id: "12-A",
+                    promoted: false,
+                },
+            ],
+            [
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+            ],
+            [
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+            ],
+            [
+                {
+                    id: "1-B",
+                    promoted: false,
+                },
+                null,
+                {
+                    id: "2-B",
+                    promoted: false,
+                },
+                null,
+                {
+                    id: "3-B",
+                    promoted: false,
+                },
+                null,
+                {
+                    id: "4-B",
+                    promoted: false,
+                },
+            ],
+            [
+                null,
+                {
+                    id: "5-B",
+                    promoted: false,
+                },
+                null,
+                {
+                    id: "6-B",
+                    promoted: false,
+                },
+                null,
+                {
+                    id: "7-B",
+                    promoted: false,
+                },
+                null,
+                {
+                    id: "8-B",
+                    promoted: false,
+                },
+            ],
+            [
+                {
+                    id: "9-B",
+                    promoted: false,
+                },
+                null,
+                {
+                    id: "10-B",
+                    promoted: false,
+                },
+                null,
+                {
+                    id: "11-B",
+                    promoted: false,
+                },
+                null,
+                {
+                    id: "12-B",
+                    promoted: false,
+                },
             ]
-        };
-        document.cookie = 'object=' + JSON.stringify(Game)
-    } else {
-        Game = JSON.parse(document.cookie.split('=')[1]);
-    }
+        ]
+    };
 
     // Get Game from backend instead of this ^^^^
 
