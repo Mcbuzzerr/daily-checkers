c118d469f835db17127e6d736d8c11e9b6925bfd
Alex Bates <alexbates.dev@gmail.com>
2024-02-20 21:49:09 -0700

    untested auth stuff

--- api/user-login/app.py
+++ api/user-login/app.py
@@ -1,21 +1,48 @@
 import boto3
-from boto3.dynamodb.conditions import Key
 from boto3.dynamodb.conditions import Attr
-from os import getenv
+import os
 import json
-from datetime import datetime
+import jwt
+from datetime import datetime, timedelta
+import hashlib
+import binascii
 
-region_name = getenv("APP_REGION")
-table = boto3.resource("dynamodb", region_name=region_name).Table("")
-
-
-def lambda_handler(event, context):
-    # path = event["pathParameters"]
-    # id = path["id"]
-    pass
+dynamodb = boto3.resource("dynamodb", region_name=os.getenv("APP_REGION"))
+table = dynamodb.Table("DailyCheckers_Users")
 
 
-def response(code, body):
+def login_handler(event, context):
+    body = json.loads(event["body"])
+    email = body["email"]
+    password = body["password"]
+
+    response = table.scan(FilterExpression=Attr("email").eq(email))
+
+    # Be wary of this line
+    user = (
+        response["Items"][0]
+        if "Items" in response and len(response["Items"]) > 0
+        else None
+    )
+
+    if "Item" not in response or not (user["password"] == password):
+        return format_response(401, {"message": "Authentication failed"})
+
+    token = generate_jwt(email)
+    user.pop("password")
+
+    return format_response(200, {"token": token, "user": user})
+
+
+def generate_jwt(email):
+    payload = {"email": email, "exp": datetime.utcnow() + timedelta(days=2)}
+    # Generate private key with: openssl genpkey -algorithm RSA -out private_key.pem -pkeyopt rsa_keygen_bits:2048
+    private_key = os.getenv("PRIVATE_KEY")
+    token = jwt.encode(payload, private_key, algorithm="RS256")
+    return token
+
+
+def format_response(code, body):
     return {
         "statusCode": code,
         "headers": {"Content-Type": "application/json"},
--- auth/user-auth/app.py
+++ auth/user-auth/app.py
@@ -3,16 +3,58 @@ from boto3.dynamodb.conditions import Key
 from boto3.dynamodb.conditions import Attr
 from os import getenv
 import json
-from datetime import datetime
+import jwt
 
 region_name = getenv("APP_REGION")
-table = boto3.resource("dynamodb", region_name=region_name).Table("")
+table = boto3.resource("dynamodb", region_name=region_name).Table("DailyCheckers_Users")
 
 
 def lambda_handler(event, context):
-    # path = event["pathParameters"]
-    # id = path["id"]
-    pass
+    auth_token = event["authorizationToken"].split(" ")[1]
+
+    # generate private key with:
+    # openssl genpkey -algorithm RSA -out private_key.pem -pkeyopt rsa_keygen_bits:2048
+    # then extract public key with:
+    # openssl rsa -pubout -in private_key.pem -out public_key.pem
+
+    public_key = getenv("PUBLIC_KEY")
+
+    decoded_token = jwt.decode(auth_token, public_key, algorithms=["RS256"])
+
+    user_or_false = found_in_db(decoded_token)
+
+    if user_or_false:
+        allow = "Allow"
+
+    response = {
+        "principalId": f"{decoded_token['email']}",
+        "policyDocument": {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Action": "execute-api:Invoke",
+                    "Effect": allow,
+                    "Resource": event["methodArn"],
+                }
+            ],
+        },
+        "context": {
+            "user": user_or_false,
+        },
+    }
+
+    return response
+
+
+def found_in_db(decoded_token):
+    print(decoded_token)
+    email = decoded_token["email"]
+    user = table.scan(FilterExpression=Attr("email").eq(email))
+
+    if len(user["Items"]) == 1:
+        return user["Items"][0]
+    else:
+        return False
 
 
 def response(code, body):
